2055a32f6aeaf816a80d4fa9179822e49389cc1d
casten01 <gabrielepaulon01@gmail.com>
2022-05-17 10:06:39 +0200

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// ===================== typing animation =====================
+
+var typed = new Typed(".typing", {
+  strings:["Genius", "Genius", "Billionaire", "Playboy", "Philanthropist"],
+  typeSpeed:100,
+   BackSpeed:60,
+   loop:true  
+})
